912d92c0e112eaa67bef5bbcd1f303958f306072
QuirinEcker <quirin.ecker@bajupa.com>
2018-03-10 15:43:47 +0100

    finish code

--- src/main/java/Ecker_PerfectNumbers.java
+++ src/main/java/Ecker_PerfectNumbers.java
@@ -1,10 +1,26 @@
 public class Ecker_PerfectNumbers {
 
     public static void main(String[] args) {
+        int ts = 0;
 
+        for (int i = 1; i <= 10000; i++) {
+            ts = teilersumme(i);
+            if (ts == i){
+                System.out.println(ts);
+            }
+        }
     }
 
     private static int teilersumme(int number) {
-        return 0;
+        int sum = 0;
+        int devisor = 1;
+
+        while (devisor < number) {
+            if (number % devisor == 0) {
+                sum += devisor;
+            }
+            devisor++;
+        }
+        return sum;
     }
 }
